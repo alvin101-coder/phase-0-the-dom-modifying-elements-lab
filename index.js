328301b03947ca16c393c032189942c9a2a36b99
Alvin Kamau <alvin.kamau1 @student.moringaschool.com>
2025-03-20 13:23:30 +0300

    done making the DOM elements

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+//create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// set the id of the <h1> to "victory"
+newHeader.id = 'victory';
+
+// Add text to the <h1>
+newHeader.innerHTML = 'Alvin is the champion';
+
+//Optionally, add the new <h1> to the DOM 
+document.body.appendChild(newHeader);
